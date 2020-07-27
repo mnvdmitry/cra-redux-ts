bd39f3e5ae31887f8f5eba7e71fee37a0c3e870a
Dmitry Minaev <me@mnvdmitry.cc>
2020-07-27 18:33:41 +0300

    Add basic styles

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import 'styles/common.css';
 import { history } from 'lib/navigation';
 import { Routes } from 'pages';
 import { configureStore } from 'store';
